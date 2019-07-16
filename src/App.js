72011653a20326e5827e5f3eeaf5347f190bc445
mykenzie <mykenzierogers@gmail.com>
2019-07-16 11:04:49 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import {Route, Switch} from 'react-router-dom';
+import Account from './components/Account';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/profile/settings" component={Account} />
+          <Route path="/profile" component={Profile} />
+          <Route path='/' render={() => <h1>404 Page Not Found! Please Contact System Administrator</h1>} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
